0d515aab98ef74bb2c0b0b4e5ab3c6c5bf09f017
owantdelhiver <owenboutet@gmail.com>
2023-03-30 18:41:59 +0200

    refacto

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import Pokemon from "./modules/Pokemon.js";
 import Attaque from "./modules/Attaque.js";
 
-let pikachu = new Pokemon("025", "Pikachu", 40, 6, "electrik", 82, [new Attaque("statik", 10), new Attaque("paratonnerre", 25)] );
-let evoli = new Pokemon("133", "Evoli", 30, 6.5, "normal", 70,[new Attaque("adaptabilité", 9), new Attaque("anticipation", 15)]);
-
+function pokeSwap() {
+    let temp = pokemons[0];
+    pokemons[0] = pokemons[1];
+    pokemons[1] = temp;
+}
 function combat(pokemons) {
     while(true) {
         console.log(`${pokemons[0].name} a ${pokemons[0].hp} points de vie.`)
@@ -13,22 +15,18 @@ function combat(pokemons) {
             break;
         }
 
-        console.log(`${pokemons[1].name} a ${pokemons[1].hp} points de vie.`)
-        pokemons[1].attaque(pokemons[0]);
-        if(pokemons[0].isDead()) {
-            console.log(`${pokemons[1].name} a gagné le combat. Il lui restait ${pokemons[1].hp} hp`);
-            break;
-        }
+        pokeSwap();
     }
 }
+
+let pikachu = new Pokemon("025", "Pikachu", 40, 6, "electrik", 82, [new Attaque("statik", 10), new Attaque("paratonnerre", 25)] );
+let evoli = new Pokemon("133", "Evoli", 30, 6.5, "normal", 70,[new Attaque("adaptabilité", 9), new Attaque("anticipation", 15)]);
 
 let pokemons = [];
 pokemons.push(pikachu, evoli);
 
 if(Math.floor(Math.random() * 2) === 1) {
-    let temp = pokemons[0];
-    pokemons[0] = pokemons[1];
-    pokemons[1] = temp;
+    pokeSwap();
 }
 
 console.log("Pikachu VS Evoli");
@@ -36,7 +34,7 @@ console.log(`Le tirage au sort a décidé que ${pokemons[0].name} attaquait en p
 console.log();
 
 for(let pokemon of pokemons) {
-    console.log(`${pokemon.name} a ${pokemon.hp} points de vie.`)
+    console.log(`${pokemon.name} a ${pokemon.hp} points de vie.`);
 }
 console.log();
 
